e4e33585e367559bff19c1c849621033e3893169
Matt Hewitt <113282375+mjh737@users.noreply.github.com>
2023-01-07 13:29:16 +0000

    MVC

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace mjh737.github.io.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
