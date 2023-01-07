8a5bb10b05f9a170a84253ad07e84a9720fdfb4d
Matt Hewitt <113282375+mjh737@users.noreply.github.com>
2023-01-07 14:16:55 +0000

    removed mvc

--- Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace mjh737.github.io.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
